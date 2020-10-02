a1264b09e217b5219e8c39269463c90a5844a244
ian cole <ianccole@gmail.com>
2020-10-02 07:36:20 +0100

    just flash and toggle power/sleep

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,11 @@
 #include <Arduino.h>
 // #include "TinyGPS.h"
 #include <ArduinoLowPower.h>
+#include <SPI.h>
+#include <RH_RF95.h>
+
+#define DIO0 17
+static RH_RF95 rf95(SS,DIO0);
 
 // TinyGPS gps;
 char buffer[128];
@@ -13,119 +18,59 @@ void setup()
     pinMode(PIN_GPS_POWER, OUTPUT);
     // digitalWrite(PIN_GPS_POWER, GPS_OFF);    // GPS power OFF
     digitalWrite(PIN_GPS_POWER, GPS_ON);    // GPS power ON
+    digitalWrite(LED_BUILTIN, HIGH); // toggle the LED on (HIGH is the voltage level)
+    // delay(8000);
+    // rf95.sleep(); // put radio into sleep mode
 }
 
-// static bool checkGPS()
-// {
-//     bool gpsFix = false;
 
-//     while (Serial1.available())
-//     {
-//         char ch;
-//         ch = Serial1.read();
-//         // SerialUSB.print(ch);
-
-//         if (gps.encode(ch))
-//         {
-//             gpsFix = true;
-//         }
-//     }
-//     return gpsFix;
-// }
-
-// static void showFix()
-// {
-//     long lat, lon;
-//     unsigned long fix_age; // returns +- latitude/longitude in degrees
-//     unsigned long date, time;
-//     gps.get_position(&lat, &lon, &fix_age);
-//     gps.get_datetime(&date, &time, &fix_age);
-//     // SerialUSB.println(fix_age);
-//     if (fix_age == TinyGPS::GPS_INVALID_AGE)
-//     {
-//         SerialUSB.println("No fix detected");
-//     }
-//     else if (fix_age > 5000)
-//     {
-//         SerialUSB.println("Warning: possible stale data!");
-//     }
-//     else
-//     {
-//         sprintf(buffer, "Date: %lu, Time: %lu, LAT: %ld, LON: %ld\n", date, time, lat, lon);
-//         SerialUSB.print(buffer);
-//     }
-// }
-enum class gps_state
+enum class led_state
 {
-    gps_on,
-    gps_on_fix,
-    gps_off
+    led_on,
+    led_off
 };
-
 
 void loop()
 {
-    static gps_state state = gps_state::gps_on_fix;
-
+    static led_state state = led_state::led_on;
 
     static unsigned long start = millis();
     static unsigned long ledtime=start;
-    static unsigned long gpstime=start;
+    static unsigned long second=start;
     static unsigned long led_interval = 1000;
     static unsigned int seconds = 0;
-    static bool gotFix = false;
-    static unsigned long ttff = 0;
 
     unsigned long now = millis();
 
-    // if(checkGPS())
-    // {
-    //     gotFix = true;
-    //     showFix();
-    // }
-
-    if (now-gpstime > 1000)
+    if (now-second > 1000)
     {
-        gpstime = now;
+        second = now;
         seconds +=1;
     }
 
     switch(state)
     {
-        case gps_state::gps_on:
-            if (gotFix)
-            {
-                state = gps_state::gps_on_fix;
-                ttff = now - start;
-                SerialUSB.print(ttff);
-                SerialUSB.println(" TTFF");
-                seconds = 0;
-                led_interval = 500;
-            }
-            break;
-
-        case gps_state::gps_on_fix:
+        case led_state::led_on:
             if (seconds > 10)
             {
-                state = gps_state::gps_off;
-                gotFix = false;
-                ttff = 0;
+                state = led_state::led_off;
                 SerialUSB.println(" gps power off");
                 digitalWrite(PIN_GPS_POWER, GPS_OFF);
                 seconds = 0;
-                led_interval = 1000;
+
+                digitalWrite(LED_BUILTIN, LOW);
                 LowPower.sleep(8000);
             }
             break;
 
-        case gps_state::gps_off:
+        case led_state::led_off:
             if (seconds > 10)
             {
-                state = gps_state::gps_on_fix;
+                state = led_state::led_on;
                 SerialUSB.println(" gps power on");
                 digitalWrite(PIN_GPS_POWER, GPS_ON);
-                start = now;
                 seconds = 0;
+                start = now;
             }
             break;
     }
